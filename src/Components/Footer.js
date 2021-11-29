494c917845a3ac9fbe80a925c8fac418dba434f4
TikvaYosef <tikvayosef13@gmail.com>
2021-11-29 15:45:40 +0200

    commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,10 +4,13 @@ import React from 'react';
 
 class Media extends React.Component {
     render() {
-        
-        return <section className = "food" ><img src = {this.props.image}  className="imagesOfFooter" />
-        <h1 className = "nameOfFood">{this.props.name}  </h1>
-        <h3 className = "discreption">{this.props.discreption} </h3>
+
+        return <section className="food" >
+            <div className ="card">
+                <img src={this.props.image} className="imagesOfFooter" />
+                <p className="nameOfFood">{this.props.name}  </p>
+                <p className="discreption">{this.props.discreption} </p>
+            </div>
         </section>
     }
 }
